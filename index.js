79c44045b54b858e03df9d42a315ddb0265d12d0
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-01-11 21:10:22 +0900

    chore: json-server axios getPosts test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('js');
+import './index.scss';
+import axios from 'axios';
+
+const getPosts = async () => {
+    const data = await axios.get('http://localhost:3000/posts');
+    console.log(data);
+    return data;
+};
+getPosts();
