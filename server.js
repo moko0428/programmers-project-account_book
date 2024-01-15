5dec25877c6ad068268ba259fcfdc5a8a9c476e3
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-01-15 21:49:40 +0900

    chore: PORT 변수 export로 재사용하기

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+export const PORT = 3003;
+
 server.use(middlewares);
 server.use(router);
-server.listen(3003, () => {
+server.listen(PORT, () => {
     console.log('JSON Server is running');
 });
